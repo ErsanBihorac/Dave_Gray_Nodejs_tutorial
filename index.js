3c27875fc82def8242346573a490263e07470065
ErsanBihorac <ersan.bihorac.2003@gmail.com>
2024-05-19 00:14:53 +0200

    build an event emitter

--- index.js
+++ index.js
@@ -1,34 +1,16 @@
-const fsPromises = require('fs').promises;
-const path = require('path');
+const logEvents = require('./logEvents');
 
-const fileOps = async () => {
-    try {
-        const data = await fsPromises.readFile(path.join(__dirname, 'files', 'starter.txt'), 'utf-8');
-        console.log(data);
-        await fsPromises.unlink(path.join(__dirname, 'files', 'starter.txt'));
-        await fsPromises.writeFile(path.join(__dirname, 'files', 'promiseWrite.txt'), data);
-        await fsPromises.appendFile(path.join(__dirname, 'files', 'promiseWrite.txt'), '\n\nNice to meet you.');
-        await fsPromises.rename(path.join(__dirname, 'files', 'promiseWrite.txt'), path.join(__dirname, 'files', 'promiseComplete.txt'));
-        const newData = await fsPromises.readFile(path.join(__dirname, 'files', 'promiseComplete.txt'), 'utf-8');
-        console.log(newData);
-    } catch (err) {
-        console.error(err);
-    }
-}
+const EventEmitter = require('events');
 
-fileOps();
+class MyEmitter extends EventEmitter { };
 
-// fs.writeFile(path.join(__dirname, 'files', 'reply.txt'), 'Nice to meet you!', (err) => {
-//     if (err) throw err;
-//     console.log('Write complete');
+//initialize object
+const myEmitter = new MyEmitter();
 
-//     fs.appendFile(path.join(__dirname, 'files', 'reply.txt'), '\n\n\nYes it is', (err) => {
-//         if (err) throw err;
-//         console.log('Append complete');
+//add listener for the log event
+myEmitter.on('log', (msg) => logEvents(msg));
 
-//         fs.rename(path.join(__dirname, 'files', 'reply.txt'), path.join(__dirname, 'files', 'newReply.txt'), (err) => {
-//             if (err) throw err;
-//             console.log('Rename complete');
-//         });
-//     });
-// });
+setTimeout(() => {
+    //Emit event
+    myEmitter.emit('log', 'Log event emitted!');
+}, 2000);
