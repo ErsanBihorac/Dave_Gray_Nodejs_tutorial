99ab8f11f0f5b7104f5834ede890fe3260b57955
ErsanBihorac <ersan.bihorac.2003@gmail.com>
2024-05-19 15:44:51 +0200

    intro to express js

--- logEvents.js
+++ logEvents.js
@@ -5,16 +5,16 @@ const fs = require('fs');
 const fsPromises = require('fs').promises;
 const path = require('path');
 
-const logEvents = async (message) => {
-    const dateTime = `${format(new Date(), 'yyyMMdd\tHH:mm:ss')}`;
+const logEvents = async (message, logName) => {
+    const dateTime = `${format(new Date(), 'yyyyMMdd\tHH:mm:ss')}`;
     const logItem = `${dateTime}\t${uuid()}\t${message}\n`;
-    console.log(logItem);
+
     try {
         if (!fs.existsSync(path.join(__dirname, 'logs'))) {
             await fsPromises.mkdir(path.join(__dirname, 'logs'));
         }
-        // testing
-        await fsPromises.appendFile(path.join(__dirname, 'logs', 'eventLog.txt'), logItem);
+
+        await fsPromises.appendFile(path.join(__dirname, 'logs', logName), logItem);
     } catch (err) {
         console.log(err);
     }
